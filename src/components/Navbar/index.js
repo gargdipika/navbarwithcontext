adb392e20a571db3da0181e0f639bb3e7119ffd8
dipikagarg <dipikagarg06@gmail.com>
2022-01-22 08:31:48 +0000

    chnage

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,43 +1,67 @@
 // Write your code here
 import {Link} from 'react-router-dom'
 import ThemeContext from '../../context/ThemeContext'
+import './index.css'
 
 const Navbar = () => (
   <ThemeContext.Consumer>
     {value => {
-      const {isDarkTheme} = value
+      const {isDarkTheme, toggleTheme} = value
+      const onClickThemeButton = () => {
+        toggleTheme()
+      }
+
+      const navBackground = isDarkTheme
+        ? 'navbar-container-dark'
+        : 'navbar-container-light'
+
+      const listStyle = isDarkTheme ? 'list-style-dark' : 'list-style-light'
       return (
-        <nav>
+        <nav className={navBackground}>
           {isDarkTheme ? (
             <img
+              className="nav-image"
               src="https://assets.ccbp.in/frontend/react-js/website-logo-dark-theme-img.png"
               alt="website logo"
             />
           ) : (
             <img
+              className="nav-image"
               src="https://assets.ccbp.in/frontend/react-js/website-logo-light-theme-img.png"
               alt="website logo"
             />
           )}
-          <ul>
-            <Link to="/">
-              <li>Home</li>
+          <ul className="unordered-list">
+            <Link className="link-style" to="/">
+              <li className={listStyle}>Home</li>
             </Link>
-            <Link to="/about">
-              <li>About</li>
+            <Link className="link-style" to="/about">
+              <li className={listStyle}>About</li>
             </Link>
           </ul>
           {isDarkTheme ? (
-            <button type="button" testid="theme">
+            <button
+              type="button"
+              testid="theme"
+              className="button"
+              onClick={onClickThemeButton}
+            >
               <img
-                src="https://assets.ccbp.in/frontend/react-js/dark-theme-img.png"
+                className="nav-image"
+                src="https://assets.ccbp.in/frontend/react-js/light-theme-img.png"
                 alt="theme"
               />
             </button>
           ) : (
-            <button type="button" testid="theme">
+            <button
+              type="button"
+              testid="theme"
+              className="button"
+              onClick={onClickThemeButton}
+            >
               <img
-                src="https://assets.ccbp.in/frontend/react-js/light-theme-img.png"
+                className="nav-image"
+                src="https://assets.ccbp.in/frontend/react-js/dark-theme-img.png"
                 alt="theme"
               />
             </button>
